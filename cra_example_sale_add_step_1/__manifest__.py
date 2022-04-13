a01f1ee1df6cad2afdcc429bf216345b836c5d07
Emiliano Abascal Gurría <emilianoabascal@me.com>
2022-04-13 17:48:03 -0500

    First example

---
+++ cra_example_sale_add_step_1/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Example CRA Module',
+    'summary': """""",
+    'description':"""
+        - Quadrigram: emab.
+        """,
+    'author': 'Odoo, Inc.',
+    'website': 'https://www.odoo.com',
+    'category':'Custom Development',
+    'version': '0.1',
+    'depends':['sale_management'],
+    'data':[
+        'views/sale_views.xml'
+    ],
+    'license': 'OPL-1',
+    } 
